5140fa8a43eb93c460cef05ff46c91313448baa3
Petr Podrouzek <petrpodrouzek@podrouzekp.local>
2017-05-04 20:45:24 +0200

    FInal version of the run_analysis.R script.

--- run_analysis.R
+++ run_analysis.R
@@ -1,45 +1,73 @@
 # This is a Getting and Cleaning Data Course Project
+# ==================================================
+# The script runs in 5 steps 
+# Step 0: Environemnt is set up
+# Step 1: Reading reusable data like feature names and assembling the column names vector
+# Step 2a: test data is loaded 
+# Step 2b: train data is loaded
+# Step 3: both data sets are merged, ordered, the tidy data set is created and printed to output.txt
+
 # Step 0: Environment set up
 # ============================
-setwd("/Users/petrpodrouzek/Documents/coursera/UCI HAR Dataset/")
+# Please to execute properly set up path to the working directory
+# In the working directory the Samsung data needs to be loaded
+# The output file is also then returned to the working directory
+setwd("/Users/petrpodrouzek/Documents/coursera/DSC_DC_Week4/")
+library(dplyr)
 
 # Step 1: Read reusable data
 # ============================
 # Reading column names from features.txt
-columnNamesData = read.table("features.txt", stringsAsFactors = FALSE, header = FALSE)
+columnNamesData = read.table("UCI HAR Dataset/features.txt", stringsAsFactors = FALSE, header = FALSE)
 nameVector <- c("featureId", "featureName")
 colnames(columnNamesData) <- nameVector
 # Save the data into a vector for a futher usage
-featureNames <- as.vector(columnNamesData[,'featureName'])
+featureNames <- as.vector(columnNamesData[,'featureName']) 
+# Final column names vecotr
+columnNames <- grep("mean\\(\\)|std\\(\\)", featureNames, value = TRUE)
+columnNames2 <- c("subjectId", "activityId", "activityName", paste(columnNames, "-MEAN", sep=""))
+columnNames <- c("subjectId", "activityId", "activityName", columnNames)
+
 # Reading activity labels
-activityData = read.table("activity_labels.txt", stringsAsFactors = FALSE, header = FALSE)
+activityData = read.table("UCI HAR Dataset/activity_labels.txt", stringsAsFactors = FALSE, header = FALSE)
 nameVector <- c("activityId", "activityName")
 colnames(activityData) <- nameVector
 
-# Step 2a: Reading test data set
-# ==============================
-testData = read.table("test/X_test.txt", stringsAsFactors = FALSE, header = FALSE)
-testActivitiesData = read.table("test/y_test.txt", stringsAsFactors = FALSE, header = FALSE)
+# Step 2a: Read test data set
+# ===========================
+testSubjects = read.table("UCI HAR Dataset/test/subject_test.txt", stringsAsFactors = FALSE, header = FALSE)
+testActivitiesData = read.table("UCI HAR Dataset/test/y_test.txt", stringsAsFactors = FALSE, header = FALSE)
+testData = read.table("UCI HAR Dataset/test/X_test.txt", stringsAsFactors = FALSE, header = FALSE)
 colnames(testData) <- featureNames
-# Add a column that says whether this is test or train data, might be useful going forward
-testData$source = rep("test", nrow(testData))
 # Add activityId
 testData$activityId = testActivitiesData[,"V1"]  
+# Add subjects
+testData$subjectId = testSubjects[, "V1"]
 # Merge the main data set with data set containing activity names
-testData <- merge(testData, activityData, by="activityId", all = TRUE)
+testData <- merge(testData, activityData, by="activityId")
+testData <- testData[,columnNames]
 
-# Step 2b: Reading train data set
-# ===============================
-trainData = read.table("train/X_train.txt", stringsAsFactors = FALSE, header = FALSE)
-trainActivitiesData = read.table("train/y_train.txt", stringsAsFactors = FALSE, header = FALSE)
+# Step 2b: Read train data set
+# ============================
+trainSubjects = read.table("UCI HAR Dataset/train/subject_train.txt", stringsAsFactors = FALSE, header = FALSE)
+trainActivitiesData = read.table("UCI HAR Dataset/train/y_train.txt", stringsAsFactors = FALSE, header = FALSE)
+trainData = read.table("UCI HAR Dataset/train/X_train.txt", stringsAsFactors = FALSE, header = FALSE)
 colnames(trainData) <- featureNames
-# Add a column that says whether this is test or train data, might be useful going forward
-trainData$source = rep("train", nrow(trainData))
 # Add activityId
 trainData$activityId = trainActivitiesData[,"V1"]  
+# Add subjects
+trainData$subjectId = trainSubjects[, "V1"]
 # Merge the main data set with data set containing activity names
-trainData <- merge(trainData, activityData, by="activityId", all = TRUE)
+trainData <- merge(trainData, activityData, by="activityId")
+trainData <- trainData[,columnNames]
 
-# Merge test and train data set
+# Step 3: Merge test and train data set
 # =============================
-
+testData <- rbind(testData, trainData)
+# Arranging the data
+testData <- arrange(testData, subjectId, activityId)
+# Applying dplyr in order to group and calculate mean
+finalData <- testData %>% group_by(subjectId, activityId, activityName) %>% summarise_each(funs(mean(., na.rm = TRUE)))
+colnames(finalData) <- columnNames2
+# Write the output to a file
+write.table(finalData, "output.txt", row.names = FALSE)
